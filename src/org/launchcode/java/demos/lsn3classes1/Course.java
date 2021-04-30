5b23f1f70f857e18c49b64002f0d2350ab9caac5
Lucas <lucasr.reehle@gmail.com>
2021-04-29 20:12:44 -0400

    exercise 4 done

--- src/org/launchcode/java/demos/lsn3classes1/Course.java
+++ src/org/launchcode/java/demos/lsn3classes1/Course.java
@@ -1,11 +1,35 @@
 package org.launchcode.java.demos.lsn3classes1;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class Course {
     String topic;
     String teacher;
     ArrayList <Student> enrolledStudents;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Course course = (Course) o;
+        return Objects.equals(topic, course.topic) && Objects.equals(teacher, course.teacher) && Objects.equals(enrolledStudents, course.enrolledStudents);
+    }
+
+public String toString(){
+    return this.teacher + " is teaching " + this.topic;
+}
+
+    public static void main(String[] args) {
+        Course someCourseProbably = new Course();
+        someCourseProbably.teacher = "Mr.Donald";
+        someCourseProbably.topic = "Learning 101";
+        System.out.println(someCourseProbably.toString());
+
+        System.out.println(someCourseProbably.topic.equals("Learning 101"));
+    }
+
+
 
 
 }
--- src/org/launchcode/java/demos/lsn3classes1/Student.java
+++ src/org/launchcode/java/demos/lsn3classes1/Student.java
@@ -3,6 +3,8 @@ package org.launchcode.java.demos.lsn3classes1;
 // Start working here with your Student class.
 // To instantiate the Student class, add your code to the main in the file, SchoolPractice.
 
+import java.util.Objects;
+
 public class Student {
 
     private String name;
@@ -38,10 +40,44 @@ public class Student {
         gpa = aGpa;
     }
 
-    Student(String name, int studentId, int numberOfCredits,double gpa){
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return studentId == student.studentId && numberOfCredits == student.numberOfCredits && Double.compare(student.gpa, gpa) == 0 && Objects.equals(name, student.name);
+    }
+
+    
+    Student(String name, int studentId, int numberOfCredits, double gpa){
         this.name = name;
         this.numberOfCredits = numberOfCredits;
         this.gpa = gpa;
         this.studentId = studentId;
     }
+    public String getGradeLevel(){
+        if(this.numberOfCredits <= 29){
+            return "Freshman";
+        } else if(this.numberOfCredits >= 30 && this.numberOfCredits <=59){
+            return "Sophomore";
+        } else if (this.numberOfCredits >= 60 && this.numberOfCredits <=89){
+            return "Junior";
+        } else {return "Senior";}
+    }
+    public void addGrade(int numOfCourseCredits,double grade){
+        double totalQualityScore = this.gpa*this.numberOfCredits;
+        totalQualityScore += grade/numOfCourseCredits;
+        this.numberOfCredits +=numOfCourseCredits;
+        this.gpa = totalQualityScore/this.numberOfCredits;
+
+    }
+
+    public static void main(String[] args) {
+
+        Student Bob = new Student("Bob", 34, 27, 2);
+        System.out.println(Bob.gpa);
+        Bob.addGrade(3,4);
+        System.out.println(Bob.gpa);
+    }
+
 }
